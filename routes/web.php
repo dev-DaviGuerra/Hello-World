0782493654ffc3bb31b9223e1711e3d90cfccb2d
DvGuerraa <davimguerra@gmail.com>
2025-04-29 14:38:05 -0300

    Parametros

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@ Route::get('/hello-world', function()  {
     return view('hello-world');
 });
 
-Route::get('hello', function() {
-    return view('hello');
+Route::get('/hello/{name}/{surname}', function($name, $surname) {
+    //return view('hello');
+    $result =  'Name: '.$name.'<br>'.'Surname: '.$surname;
+    return $result;
+});
+
+Route::get('/hello/{name}', function($name) {
+    //return view('hello');
+    $result =  'Name: '.$name.'<br>';
+    return $result;
 });
